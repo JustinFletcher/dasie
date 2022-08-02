0db5f6eb48f4169e4a494d88e7962246f5a6cb65
Justin Fletcher <justinr.fletcher@gmail.com>
2022-08-01 16:52:48 -1000

    Added pupil mask.

--- differentiable_dasie_graph.py
+++ differentiable_dasie_graph.py
@@ -269,7 +269,11 @@ def zernike_aperture_function_2d(X, Y, mu_u, mu_v, aperture_radius, subaperture_
 
         print("End of Zernike Term.")
 
-
+    T_alpha = tf.ones_like(X) * alpha
+    T_beta = tf.ones_like(X) * beta
+    T = (X, Y, T_mu_u, T_mu_v, T_alpha, T_beta)
+    pupil_mask = tf.vectorized_map(tensor_generalized_gaussian_2d, T)
+    tensor_zernike_2d_sample = tensor_zernike_2d_sample * pupil_mask
     # The piston tip and tilt are encoded as the phase-angle of pupil plane
     print("Generating phase angle field.")
     tensor_zernike_2d_field = tf.exp(tensor_zernike_2d_sample)
