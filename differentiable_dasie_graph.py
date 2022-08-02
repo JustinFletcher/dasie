a3f1fb54a9ccf23898059a9d5d98c9d4f9b10ed7
Justin Fletcher <justinr.fletcher@gmail.com>
2022-08-01 16:22:37 -1000

    Zernike ttp debug.

--- differentiable_dasie_graph.py
+++ differentiable_dasie_graph.py
@@ -303,7 +303,7 @@ def zernike_aperture_function_2d(X, Y, mu_u, mu_v, aperture_radius, subaperture_
     # print(aperture_sample)
 
     print("Ending aperture function.")
-    return aperture_sample
+    return tensor_zernike_2d_field
 
 class DASIEModel(object):
 
