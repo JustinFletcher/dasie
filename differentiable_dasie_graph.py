6021ff312aad44d31c28f0bc5da9fe32ef1d01b2
Justin Fletcher <justinr.fletcher@gmail.com>
2022-08-13 15:35:11 -1000

    Adjusted conditioning for quadrafoils.

--- differentiable_dasie_graph.py
+++ differentiable_dasie_graph.py
@@ -255,7 +255,7 @@ def zernike_6(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(8.0, dtype=tf.float64)) * (cartesian_radial ** 3.0) * tf.math.sin(3.0 * cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
@@ -271,7 +271,7 @@ def zernike_7(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(8.0, dtype=tf.float64)) * ((3.0 * (cartesian_radial**3)) - (2.0 * cartesian_radial)) * tf.math.sin(cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
@@ -288,7 +288,7 @@ def zernike_8(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(8.0, dtype=tf.float64)) * ((3.0 * (cartesian_radial**3)) - (2.0 * cartesian_radial)) * tf.math.cos(cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
@@ -304,7 +304,7 @@ def zernike_9(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(8.0, dtype=tf.float64)) * (cartesian_radial ** 3) * tf.math.cos(3.0 * cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
@@ -337,7 +337,7 @@ def zernike_11(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(10.0, dtype=tf.float64)) * ((4.0 * (cartesian_radial ** 4)) - (3.0 * (cartesian_radial ** 2))) * tf.math.sin(2.0 * cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
@@ -353,7 +353,7 @@ def zernike_12(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(10.0, dtype=tf.float64)) * ((6.0 * (cartesian_radial ** 4)) - (6.0 * (cartesian_radial ** 2)) + 1.0)
     z = tf.cast(z, dtype=tf.complex128)
@@ -369,7 +369,7 @@ def zernike_13(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(10.0, dtype=tf.float64)) * ((4.0 * (cartesian_radial ** 4)) - (3.0 * (cartesian_radial ** 2))) * tf.math.cos(2.0 * cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
@@ -384,7 +384,7 @@ def zernike_14(T):
     v_field = tf.cast(v, dtype=tf.float64) - mu_v
     cartesian_radial = tf.math.sqrt(u_field**2 + v_field**2)
     # Numerically condition the radial field to prevent NaNs.
-    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(10.0, dtype=tf.float64))
+    cartesian_radial = tf.math.minimum(cartesian_radial, tf.constant(5.0, dtype=tf.float64))
     cartesian_azimuth = tf.math.atan2(v_field, u_field)
     z = tf.math.sqrt(tf.constant(10.0, dtype=tf.float64)) * (cartesian_radial ** 4) * tf.math.cos(4 * cartesian_azimuth)
     z = tf.cast(z, dtype=tf.complex128)
