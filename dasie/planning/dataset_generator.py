Lots of bug fixes. Working, save for physical units.

dasie/planning/dataset_generator.py
@@ -240,8 +240,11 @@ class DatasetGenerator(object):
             data = data.map(self._rotate_random,
                             num_parallel_calls=num_threads)
 
-        # Crop the data to a specified size.
-        if crop_size:
+        image_batch_shape = get_input_shape(data)
+        if crop_size and ((crop_size < image_batch_shape[0])
+                          and
+                          (crop_size < image_batch_shape[1])):
+
 
             data = data.map(self._perform_center_crop,
                             num_parallel_calls=num_threads).prefetch(buffer)
@@ -404,6 +407,7 @@ class DatasetGenerator(object):
         crop_width = self.spatial_quantization
         crop_height = self.spatial_quantization
 
+
         # Now come up with crop offsets
         offset_width = tf.random.uniform(
             [],
@@ -420,6 +424,7 @@ class DatasetGenerator(object):
 
         # The heavy lifting is done, time to make us a crop and transform our
         # bounding boxes to the new coordinates
+
         image = tf.image.crop_to_bounding_box(image,
                                               (img_shape[0] // 2) - (
                                                           offset_height // 2),
@@ -427,6 +432,7 @@ class DatasetGenerator(object):
                                                           offset_width // 2),
                                               crop_height,
                                               crop_width)
+
 
         if filename is not None:
             return image, filename

dasie/planning/train.py
@@ -368,7 +368,7 @@ def main(flags):
 
     # Physical computations from flags.
     ap_radius_meters = (flags.aperture_diameter_meters / 2)
-    subap_radius_meters = (((ap_radius_meters / ((1.0 + flags.edge_padding_factor) / 2)) * np.sin(np.pi / flags.num_subapertures)) - (flags.subaperture_spacing_meters / 2)) / (1 + np.sin(np.pi / flags.num_subapertures))
+    subap_radius_meters = (((ap_radius_meters / ((1.0 + flags.edge_padding_factor))) * np.sin(np.pi / flags.num_subapertures)) - (flags.subaperture_spacing_meters / 2)) / (1 + np.sin(np.pi / flags.num_subapertures))
     subap_area = np.pi * (subap_radius_meters ** 2)
     total_subap_area = subap_area * flags.num_subapertures
     mono_ap_area = np.pi * (ap_radius_meters ** 2)
@@ -398,6 +398,11 @@ def main(flags):
         parse_function = speedplus_parse_function
         train_data_dir = os.path.join(flags.dataset_root, "speedplus_tfrecords", "train")
         valid_data_dir = os.path.join(flags.dataset_root, "speedplus_tfrecords", "valid")
+
+    elif flags.dataset_name == "speedplus_synthetic":
+        parse_function = speedplus_parse_function
+        train_data_dir = os.path.join(flags.dataset_root, "speedplus_synthetic_tfrecords", "train")
+        valid_data_dir = os.path.join(flags.dataset_root, "speedplus_synthetic_tfrecords", "valid")
 
 
     elif flags.dataset_name == "inria_holiday":
@@ -437,6 +442,8 @@ def main(flags):
         tf.summary.experimental.set_step(step)
         writer = tf.summary.create_file_writer(save_dir)
 
+
+
         print("\n\n\n\n\n\n\n\n\n Building Dataset... \n\n\n\n\n\n\n\n\n")
         # Build our datasets.
         train_dataset = DatasetGenerator(train_data_dir,
@@ -465,6 +472,8 @@ def main(flags):
                                          cache_dataset_memory=False,
                                          cache_dataset_file=False,
                                          cache_path="")
+
+
         print("\n\n\n\n\n\n\n\n\n Dataset Built... \n\n\n\n\n\n\n\n\n")
 
         # Get the image shapes stored during dataset construction.
@@ -544,7 +553,22 @@ def main(flags):
             object_distance_meters=object_distance_meters,
             zernike_init_type=flags.zernike_init_type,
             filter_wavelength_micron=flags.filter_wavelength_micron,
+            sensor_gaussian_mean=flags.sensor_gaussian_mean,
+            sensor_poisson_mean_arrival=flags.sensor_poisson_mean_arrival,
+            dm_stroke_microns=flags.dm_stroke_microns,
+            focal_extent_meters=flags.focal_extent_meters,
+            r0_mean=flags.r0_mean,
+            r0_std=flags.r0_std,
+            outer_scale_mean=flags.outer_scale_mean,
+            outer_scale_std=flags.outer_scale_std,
+            inner_scale_mean=flags.inner_scale_mean,
+            inner_scale_std=flags.inner_scale_std,
+            greenwood_time_constant_sec_mean=flags.greenwood_time_constant_sec_mean,
+            greenwood_time_constant_sec_std=flags.greenwood_time_constant_sec_std,
+            effective_focal_length_meters=flags.effective_focal_length_meters,
         )
+
+
 
         # Merge all the summaries from the graphs, flush and init the nodes.
         all_summary_ops = tf.compat.v1.summary.all_v2_summary_ops()
@@ -687,7 +711,8 @@ if __name__ == '__main__':
 
     # One arcminute = 0.0166667. 67 arcminutes is 0.0186111.
     # 0.0001145 ~= 1m at 1 Mm (LEO)
-    parser.add_argument('--field_of_view_degrees', type=float,
+    parser.add_argument('--field_of_view_degrees',
+                        type=float,
                         default=0.0001145,
                         help='The FOV of the optical system in degrees.')
 
@@ -712,7 +737,84 @@ if __name__ == '__main__':
                         help='The wavelength of monochromatic light used \
                               in this simulation.')
 
+    parser.add_argument('--sensor_gaussian_mean',
+                        type=float,
+                        default=1e-5,
+                        help='The mean for the Gaussian sensor noise.')
 
+    parser.add_argument('--sensor_poisson_mean_arrival',
+                        type=float,
+                        default=4e-5,
+                        help='The Poisson mean arrival time for sensor noise.')
+
+    parser.add_argument('--dm_stroke_microns',
+                        type=float,
+                        default=8.0,
+                        help='The full stroke of the modeled DM in microns')
+
+    parser.add_argument('--focal_extent_meters',
+                        type=float,
+                        default=0.1,
+                        help='The extent of the square focal plane in meters.')
+
+    parser.add_argument('--r0_mean',
+                        type=float,
+                        default=0.020,
+                        help='The mean of the normal distribution of r0.')
+
+    parser.add_argument('--r0_std',
+                        type=float,
+                        default=0.0,
+                        help='The std of the normal distribution of r0.')
+
+    parser.add_argument('--outer_scale_mean',
+                        type=float,
+                        default=2000.0,
+                        help='The std of the normal distribution of outer \
+                              scale for the Von Karman atmosphere model. \
+                              see: https://arxiv.org/ftp/arxiv/papers/ \
+                              1112/1112.6033.pdf')
+
+    parser.add_argument('--outer_scale_std',
+                        type=float,
+                        default=0.0,
+                        help='The std of the normal distribution of outer \
+                              scale for the Von Karman atmosphere model. \
+                              see: https://arxiv.org/ftp/arxiv/papers/ \
+                              1112/1112.6033.pdf')
+
+    parser.add_argument('--inner_scale_mean',
+                        type=float,
+                        default=0.008,
+                        help='The mean of the normal distribution of inner \
+                              scale for the Von Karman atmosphere model. \
+                              see: https://arxiv.org/ftp/arxiv/papers/ \
+                              1112/1112.6033.pdf')
+
+    parser.add_argument('--inner_scale_std',
+                        type=float,
+                        default=0.0,
+                        help='The std of the normal distribution of inner \
+                              scale for the Von Karman atmosphere model. \
+                              see: https://arxiv.org/ftp/arxiv/papers/ \
+                              1112/1112.6033.pdf')
+
+    parser.add_argument('--greenwood_time_constant_sec_mean',
+                        type=float,
+                        default=1.0,
+                        help='The mean of the normal distribution of the \
+                              Greenwood time constant.')
+
+    parser.add_argument('--greenwood_time_constant_sec_std',
+                        type=float,
+                        default=0.0,
+                        help='The std of the normal distribution of the \
+                              Greenwood time constant.')
+
+    parser.add_argument('--effective_focal_length_meters',
+                        type=float,
+                        default=726.0,
+                        help='The effective focal length in meters.')
 
 
     # Parse known arguments.

dasie/planning/zernike.py
@@ -400,7 +400,7 @@ def zernike_aperture_function_2d(X,
     # Map the zernike domain from [-1, 1] to [0, 1], per term
     # Each term can contribute as much as -1, and has a range of size 2.
     # TODO: Ryan, talk with me about this. It changes the mapping from Zernike
-    # TODO: coefficients to Z in a way that might be problematic on the bench.
+    #       coefficients to Z in a way that might be problematic on the bench.
     num_terms = len(zernike_coefficients)
     tensor_zernike_2d_sample = (tensor_zernike_2d_sample + num_terms) / (2 * num_terms)
 
@@ -410,9 +410,9 @@ def zernike_aperture_function_2d(X,
     pupil_mask = tf.cast(tf.constant(pupil_mask), dtype=tf.complex128)
     tensor_masked_zernike_2d_sample = tensor_zernike_2d_sample * pupil_mask
 
-    # The piston tip and tilt are encoded as the phase-angle of pupil plane
-    print("-Generating phase angle field.")
     # TODO: Reinstate after debug? Talk to Ryan: Why should I do this?
+    # The piston tip and tilt are encoded as the phase-angle of pupil plane
+    # print("-Generating phase angle field.")
     # Normalize the zernike field so that it may be returned as
     # zernike_min = tf.cast(tf.math.reduce_min(tf.math.abs(tensor_masked_zernike_2d_sample)), dtype=tf.complex128)
     # zernike_max = tf.cast(tf.math.reduce_max(tf.math.abs(tensor_masked_zernike_2d_sample)), dtype=tf.complex128)

dasie/utils/make_speedplus_tfrecords.py
@@ -116,14 +116,51 @@ def _floats_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 
+def crop_center(img, crop_x, crop_y):
+    y, x = img.shape
+    start_x = x // 2 - crop_x // 2
+    start_y = y // 2 - crop_y // 2
+    return img[start_y:start_y + crop_y, start_x:start_x + crop_x]
+
 def build_speedplus_tf_example(example):
 
     (image_path) = example
 
     # Read in the files for this example
     image = read_jpg(image_path)
+
+    image = crop_center(image, 512, 512)
+
     image_height = image.shape[1]
     image_width = image.shape[0]
+    # Ada: You'll need to read from annotation_path to get, e.g., class labels.
+
+    # Create the features for this example
+    # Ada: Note here how I'm serializing the image and then binarizing it.
+    features = {
+        "image_raw": _bytes_feature([image.tostring()]),
+        "height": _int64_feature([image_height]),
+        "width": _int64_feature([image_width]),
+        # Ada: Annotation mappings go here - I just don't need any right now...
+        # "class_label": _int64_feature(class_label)
+    }
+
+
+    # Create an example protocol buffer
+    example = tf.train.Example(features=tf.train.Features(feature=features))
+
+    return(example)
+
+def build_imagenet_tf_example(example):
+
+    # TODO: inlcude synset or class annotations.
+    (image_path) = example
+
+    # Read in the files for this example
+    image = read_jpg(image_path)
+    image_height = image.shape[1]
+    image_width = image.shape[0]
+
 
     # Ada: You'll need to read from annotation_path to get, e.g., class labels.
 
@@ -142,6 +179,7 @@ def build_speedplus_tf_example(example):
     example = tf.train.Example(features=tf.train.Features(feature=features))
 
     return(example)
+
 
 
 def group_list(ungrouped_list, group_size, padding=None):
@@ -168,7 +206,6 @@ def get_immediate_subdirectories(a_dir):
 
 
 def build_speedplus_dataset(datapath):
-
 
     # We're going to make a list of filepaths as a template.
     examples = list()
@@ -362,15 +399,22 @@ def get_dir_content_paths(directory):
 
 def main(flags):
 
-    # If one desires a deterministic split, pass in a splits file.
-    if flags.splits_files_path:
-        split_dict = flags.splits_files_path
-        partition_fn = partition_examples_by_file
+    if flags.presplit:
 
-    # Otherwise, we'll just default to a 8/1/1 at random.
-    else:
-        split_dict = {"train": 0.8, "valid": 0.1, "test": 0.1}
+        split_dict = {"data": 1.0}
         partition_fn = partition_examples
+
+    else:
+
+        # If one desires a deterministic split, pass in a splits file.
+        if flags.splits_files_path:
+            split_dict = flags.splits_files_path
+            partition_fn = partition_examples_by_file
+
+        # Otherwise, we'll just default to a 8/1/1 at random.
+        else:
+            split_dict = {"train": 0.8, "valid": 0.1, "test": 0.1}
+            partition_fn = partition_examples
 
     # TODO: externalize this function interface.
     datapath_fn = build_speedplus_dataset
@@ -402,17 +446,27 @@ if __name__ == '__main__':
     #                     default="C:\\Users\\justin.fletcher\\research\\speedplus_tfrecords",
     #                     help='Path to the output directory.')
 
+    #
+    # parser.add_argument('--data_dir', type=str,
+    #                     default="C:\\Users\\justin.fletcher\\research\\data\\speedplus_one\\speedplus",
+    #                     help='Path to speedplus output data.')
+    #
+    # parser.add_argument('--output_dir', type=str,
+    #                     default="C:\\Users\\justin.fletcher\\research\\data\\speedplus_one_tfrecords",
+    #                     help='Path to the output directory.')
+    #
+
 
     parser.add_argument('--name', type=str,
-                        default="inria_holiday",
+                        default="speedplus_synthetic_custom_30k",
                         help='Name of the dataset to build.')
 
     parser.add_argument('--data_dir', type=str,
-                        default="C:\\Users\\justin.fletcher\\research\\data\\speedplus_one\\speedplus",
+                        default="C:\\Users\\justin.fletcher\\research\\data\\speedplus_synthetic\\speedplus",
                         help='Path to speedplus output data.')
 
     parser.add_argument('--output_dir', type=str,
-                        default="C:\\Users\\justin.fletcher\\research\\data\\speedplus_one_tfrecords",
+                        default="C:\\Users\\justin.fletcher\\research\\data\\speedplus_synthetic",
                         help='Path to the output directory.')
 
     parser.add_argument("--examples_per_tfrecord",
@@ -425,6 +479,10 @@ if __name__ == '__main__':
                         default=False,
                         help='If true, map rgb jpgs/pngs to NTSC greyscale.')
 
+    parser.add_argument("--presplit",
+                        action='store_true',
+                        default=False,
+                        help='If true, assume that subdirs define splits.')
 
     parser.add_argument("--splits_files_path", type=str,
                         default=None,